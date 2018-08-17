09107f3897dce9fcbbfcba888b3e823e309224c4
Patrick Muff <muff.patrick@muffag.ch>
2018-08-17 10:55:24 +0200

    Start using a proper queue for conversion

--- watch.js
+++ watch.js
@@ -6,11 +6,15 @@ const fs = require('fs');
 
 // Make sure that OUTPUT_DIRECTORY is not within WATCH_DIRECTORY or you will
 // encounter an inifinite loop.
-const WATCH_DIRECTORY = path.resolve('./input');
-const OUTPUT_DIRECTORY = path.resolve('./output');
+const WATCH_DIRECTORY = path.resolve(process.env.CONVERT_WATCH_DIR || './input');
+const OUTPUT_DIRECTORY = path.resolve(process.env.CONVERT_OUT_DIR || './output');
 
+console.log('Initializing...');
+console.log('WATCH_DIRECTORY', WATCH_DIRECTORY);
+console.log('OUTPUT_DIRECTORY', OUTPUT_DIRECTORY);
 
-let watcher = chokidar.watch(WATCH_DIRECTORY, {
+// Setup chokidar watcher
+const watcher = chokidar.watch(WATCH_DIRECTORY, {
   ignored: /(^|[\/\\])\../,
   persistent: true,
   ignoreInitial: true,
@@ -20,30 +24,91 @@ let watcher = chokidar.watch(WATCH_DIRECTORY, {
   }
 });
 
+// `fileQueue` is an array of all added file paths that will be sequentially
+// converted.
+let fileQueue = [];
+let queueIsWorking = false;
 
 watcher.on('add', (filePath) => {
+  fileQueue.push(filePath);
+
+  if (!queueIsWorking) {
+    startQueue();
+  }
+});
+
+/**
+ * Starts working on the file queue.
+ */
+async function startQueue() {
+  queueIsWorking = true;
+
+  while (fileQueue.length > 0) {
+    let filePath = fileQueue.pop();
+    await startJobs(filePath);
+  }
+
+  queueIsWorking = false;
+}
+
+/**
+ * Starts the jobs defined in `jobs.json` for the given `filePath`. It also
+ * creates statistics for each job and adds them to the output directory as
+ * `stats.txt`.
+ * @param {string} filePath Full path to the file
+ */
+async function startJobs(filePath) {
   const extension = path.extname(filePath);
   const fileName = path.basename(filePath, extension);  
 
   // create directory in output folder with the same name as the file
-  const targetDirectory = path.join(OUTPUT_DIRECTORY, fileName);
-  if (!fs.existsSync(targetDirectory)) {
-    fs.mkdirSync(targetDirectory);
+  const outDir = fileName + '_' + Math.floor(Math.random() * Date.now());
+  const outDirPath = path.join(OUTPUT_DIRECTORY, outDir);
+  if (!fs.existsSync(outDirPath)) {
+    fs.mkdirSync(outDirPath);
   }
   
-  for (let job of jobs) {
-    const outputFileName = `${fileName}_${job.name}${job.out_ext}`;
-    const outputPath = path.join(OUTPUT_DIRECTORY, fileName, outputFileName);
+  let stats = [];
 
-    const command = `ffmpeg -i "${filePath}" ${job.flags} -y "${outputPath}"`;
+  for (let job of jobs) {
+    const outFileName = `${fileName}_${job.name}${job.out_ext}`;
+    const outFilePath = path.join(outDirPath, outFileName);
+
+    const command = `ffmpeg -i "${filePath}" ${job.flags} -y "${outFilePath}"`;
 
     console.log('Starting job', job.name);
 
+    try {
+      let start = Date.now();
+      await executeCommand(command);
+      let duration = Math.round((Date.now() - start) / 1000);
+      stats.push({
+        job: job.name,
+        input: filePath,
+        output: outFilePath,
+        duration_in_seconds: duration
+      });
+      console.log('Completed job', job.name);
+    } catch (ex) {
+      console.log('Job failed', ex);
+    }
+  }
+
+  // Write statistics file
+  fs.writeFileSync(path.join(outDirPath, 'stats.txt'), JSON.stringify(stats, null, 2));
+};
+
+/**
+ * Spawns a command using child_process.exec() and returns a Promise.
+ * @param {string} command Terminal command
+ */
+async function executeCommand(command) {
+  return new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
       if (error) {
-        console.log(error);
+        return reject(error);
       }
-      console.log('Completed job', job.name);
+      resolve(stdout);
     });
-  }
-});
+  });
+}
